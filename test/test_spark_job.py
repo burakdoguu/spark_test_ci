82acd61cd7f0764e8856e5b5c5a6688c6833056a
Burak Samed Doğu <burakdoguu1@gmail.com>
2023-11-16 21:18:04 +0300

    test

--- test/test_spark_job.py
+++ test/test_spark_job.py
@@ -2,7 +2,7 @@ import pytest
 from pyspark.sql import SparkSession
 import sys
 
-from spark_job_re import BatchProcess
+from app.spark_job_re import BatchProcess
 
 @pytest.fixture(scope='session')
 def spark_session():
@@ -18,4 +18,7 @@ def test_column_count(spark_session):
     expected_df = len(check_df.columns)
 
     assert expected_df == 9
+
+
+
 
